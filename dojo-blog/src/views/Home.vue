5ccd3cb8a0facab44665530f30b559f90fa203d4
Joshua Perez Leduc <joshuapleduc@gmail.com>
2021-05-23 20:17:28 -0400

    dojo-blog

---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
+  </div>
+</template>
+
+<script>
+import getPosts from "../composables/getPosts";
+import PostList from "../components/PostList.vue";
+
+export default {
+  name: "Home",
+  components: { PostList },
+  setup() {
+    const { posts, error, load } = getPosts();
+    load();
+    return { posts, error };
+  },
+};
+</script>
+
+<style>
+  .home {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 10px;
+  }
+  .layout {
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    gap: 20px;
+  }
+</style>
+
